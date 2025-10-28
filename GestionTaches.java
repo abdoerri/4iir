60cca5f300b13b1995aeae61d85bc3588d0e4bf1
abdoerri <Abderrahman.Erritouni@emsi-edu.ma>
2025-10-28 11:14:31 +0100

    Add files via upload

---
+++ GestionTaches.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+class GestionTaches {
+    private LinkedList<Tache> taches = new LinkedList<>();
+
+    public void ajouterTache(Tache tache) {
+        taches.add(tache);
+    }
+
+    public void terminerPremiereTache() {
+        if (!taches.isEmpty()) {
+            taches.getFirst().terminer();
+        }
+    }
+
+    public void afficherTaches() {
+        for (Tache t : taches) {
+            System.out.println(t);
+        }
+    }
+
+    public static void main(String[] args) {
+        GestionTaches gt = new GestionTaches();
+        gt.ajouterTache(new Tache("Faire les devoirs"));
+        gt.ajouterTache(new Tache("Préparer la présentation"));
+        gt.terminerPremiereTache();
+        gt.afficherTaches();
+    }
+}
