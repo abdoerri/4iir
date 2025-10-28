9be8aebc9538ee9912018b19d3a9b0de44edece7
abdoerri <Abderrahman.Erritouni@emsi-edu.ma>
2025-10-28 11:05:43 +0100

    Add files via upload

---
+++ Livre.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Livre {
+    private String titre;
+    private String auteur;
+    private int annee;
+
+    public Livre(String titre, String auteur, int annee) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.annee = annee;
+    }
+
+    public String getTitre() { return titre; }
+    public String getAuteur() { return auteur; }
+    public int getAnnee() { return annee; }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+               "titre='" + titre + '\'' +
+               ", auteur='" + auteur + '\'' +
+               ", annee=" + annee +
+               '}';
+    }
+}
+
+
+
+   
